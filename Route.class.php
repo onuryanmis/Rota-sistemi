17d04a69171b465287a97d6e22fffab22b496b6a
onuryanmis <onur_erzincan1999@hotmail.com>
2016-07-30 23:03:02 +0300

    Update Route.class.php

--- Route.class.php
+++ Route.class.php
@@ -11,6 +11,7 @@ class Route
 {
 	private static $error = true;
 	private static $extension = null;
+	private static $case_sensitive = true;
 
 	/**
 	 * Route settings
@@ -20,7 +21,14 @@ class Route
 	{
 		if(is_array($config))
 		{
-			self::$extension = $config["extension"];
+			if(isset($config["extension"]))
+			{
+				self::$extension = $config["extension"];
+			}
+			if(isset($config["case_sensitive"]))
+			{
+				self::$case_sensitive = $config["case_sensitive"];
+			}		
 		}else
 		{
 			echo "<font color='red'>Config metodu parametre olarak sadece dizi değerler alabilir!</font>";
@@ -107,8 +115,19 @@ class Route
 		}
 		
 		$path_info = empty($_SERVER["PATH_INFO"]) ? "/" : $_SERVER["PATH_INFO"];
+
+		$pattern = str_replace("(:any)", "(.*?)", $pattern);
+		$pattern = str_replace("(:number)", "(\d+)", $pattern);
+
+		if(self::$case_sensitive == false)
+		{
+			$pattern = "#^$pattern$#";
+		}else
+		{
+			$pattern = "#^$pattern$#i";
+		}
 		
-		if(preg_match("#^$pattern$#",$path_info, $param) &&  $_SERVER["REQUEST_METHOD"]==$method)
+		if(preg_match($pattern,$path_info, $param) &&  $_SERVER["REQUEST_METHOD"]==$method)
 		{
 
 			if(is_callable($function))
